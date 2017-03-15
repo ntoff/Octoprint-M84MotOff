81f37e3e4d50e52ee6c81638e208412bf0033355
ntoff <ntoff@users.noreply.github.com>
2017-03-15 20:25:54 +1000

    simplify the code
    no more class

--- octoprint_m84motoff/__init__.py
+++ octoprint_m84motoff/__init__.py
@@ -1,22 +1,15 @@
 # coding=utf-8
+from __future__ import absolute_import
 
-import octoprint.plugin
-
-class M84MotOffPlugin(octoprint.plugin.OctoPrintPlugin):
-        
-    def rewrite_m18(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-        if gcode and gcode == "M18":
-            cmd = "M84"
-        return cmd,
+def rewrite_m18(comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+    if gcode and gcode == "M18":
+        cmd = "M84"
+    return cmd,
 
 __plugin_name__ = "M84 Motors Off"
 
-def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = M84MotOffPlugin()
+__plugin_hooks__ = {
+        "octoprint.comm.protocol.gcode.queuing": rewrite_m18
+}
 
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_m18
-    }
     
