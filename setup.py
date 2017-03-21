4a111d7007934cc40f362360d809468eacd7fd08
ntoff <ntoff@users.noreply.github.com>
2017-03-21 20:58:36 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Changes the 'Motors off' button to issue the command M84 (for compatibility with Repetier firmware)."""
+plugin_description = """Changes the 'Motors off' command to M84 (for compatibility with Repetier firmware)."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "ntoff"
