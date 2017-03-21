a6a0df180e5508e552187d8a4dd6b8b2ea5c62c4
ntoff <ntoff@users.noreply.github.com>
2017-03-21 19:35:42 +1000

    Update __init__.py
    Gone back to being a bit more complicated to force a restart

--- octoprint_m84motoff/__init__.py
+++ octoprint_m84motoff/__init__.py
@@ -1,15 +1,15 @@
 # coding=utf-8
 from __future__ import absolute_import
+import octoprint.plugin
 
-def rewrite_m18(comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-    if gcode and gcode == "M18":
-        cmd = "M84"
-    return cmd,
-
+class M84MotOffPlugin(octoprint.plugin.RestartNeedingPlugin):
+    def rewrite_m18(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+	    if gcode and gcode == "M18":
+	        cmd = "M84"
+	    return cmd,
+        
 __plugin_name__ = "M84 Motors Off"
-
+__plugin_implementation__ = M84MotOffPlugin()
 __plugin_hooks__ = {
-        "octoprint.comm.protocol.gcode.queuing": rewrite_m18
-}
-
-    
+        "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_m18
+    }  
